7c55636d8d40a54fcedba1e72783511de40db566
Anjelo Esperanzate <Anjelo Esperanzate>
2025-10-23 14:53:56 +0800

    Fixed auth-guard.js

--- js/auth-guard.js
+++ js/auth-guard.js
@@ -1,19 +1,17 @@
 function protectPage(expectedRole) {
   const userData = JSON.parse(localStorage.getItem("userData"));
   const userRole = localStorage.getItem("userRole");
-  const LOGIN_PAGE = window.location.origin + "/login.html";
-  window.location.href = LOGIN_PAGE;
 
   // Redirect if not logged in
   if (!userData || !userRole) {
-    window.location.href = LOGIN_PAGE;
+    window.location.href = "../../../login.html";
     return;
   }
 
   // Block if wrong role
   if (userRole !== expectedRole) {
     alert("Access denied!");
-    window.location.href = LOGIN_PAGE;
+    window.location.href = "../../../login.html";
     return;
   }
 
